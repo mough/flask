1856316bfc8ccb55bde7774b8796ff537f5fa54a
Mo <mmslavin@gmail.com>
2018-01-04 13:12:17 +0000

    updating sams working changes before I edit

--- flask_app.py
+++ flask_app.py
@@ -1,48 +1,9 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request
 from sqlalchemy import create_engine
+import json
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
-
-# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-#     username="mslavin",
-#     password="iamnotspam",
-#     hostname="mslavin.mysql.pythonanywhere-services.com",
-#     databasename="mslavin$MyData")
-
-# SQLALCHEMY_BINDS = {
-#     # comments: "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-#     # username="mslavin",
-#     # password="iamnotspam",
-#     # hostname="mslavin.mysql.pythonanywhere-services.com",
-#     # databasename="mslavin$comments")
-
-#     recipe_data: "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-#     username="mslavin",
-#     password="iamnotspam",
-#     hostname="mslavin.mysql.pythonanywhere-services.com",
-#     databasename="mslavin$MyData")
-#     }
-# app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-# app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# db = SQLAlchemy(app)
-
-# class Comment(db.Model):
-#     __bind_key__ = "comments"
-#     id = db.Column(db.Integer, primary_key=True)
-#     content = db.Column(db.String(4096))
-
-# class Recipes(db.Model):
-#     __bind_key__ = "recipes"
-#     id = db.Column(db.Integer, primary_key=True)
-#     recipe_name = db.Column(db.String(100))
-#     recipe_link = db.Column(db.String(100))
-
 
 recipe_data = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="mslavin",
@@ -50,32 +11,18 @@ recipe_data = "mysql+mysqlconnector://{username}:{password}@{hostname}/{database
     hostname="mslavin.mysql.pythonanywhere-services.com",
     databasename="mslavin$MyData")
 
-engine = create_engine(recipe_data, convert_unicode=True)
-comments = []
-results = []
-
 @app.route('/', methods=["GET"])
 def index():
-    if request.method == "GET":
-        results = engine.execute('select * from RecipeData;')
-        return render_template("main_page.html", results=results)
-        #return render_template("main_page.html", comments=Comment.query.all(), results=results)
+    return render_template("main_page.html")
 
-# @app.route('/add_comment/', methods=['POST'])
-# def add_comment():
-#     comment = Comment(content=request.form["comment_text"])
-#     db.session.add(comment)
-#     db.session.commit()
-#     return redirect(url_for('index'))
-
-@app.route('/get_recipes/', methods=['POST'])
+@app.route('/recipes', methods=["GET"])
 def get_recipes():
-    zip_code = request.form["zip_code"]
-    cuisine_pref = request.form["cuisine_pref"]
-    return "zip_code: %s  cuisine_pref: %s" % (zip_code, cuisine_pref)
-    #return redirect(url_for('index'))
+    title = request.args.get("name")
+    category = request.args.get("category")
+    url= request.args.get("url")
 
-
-@app.route('/bananas')
-def hello_bananas():
-    return 'Hello from Flask! Also, bananas!'
+    engine = create_engine(recipe_data)
+    sql_query_string = "select * from RecipeData where recipe_name LIKE '%{0}%' OR recipe_link = '{1}' OR category = '{2}';".format(title, url, category)
+    print(sql_query_string)
+    results = engine.execute(sql_query_string)
+    return json.dumps([(dict(row.items())) for row in results])
