8da380bcbd97537bf724dfe4f3aa97ac7c78e272
mslavin <mmslavin@gmail.com>
2018-04-08 16:39:45 +0000

    adding limit to sql query

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from sqlalchemy import create_engine
+#from car_data import get_cars_by  # Week 12 Day 1 Sample Demo code
 import json
 import sys
 
@@ -8,10 +9,38 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 recipe_data = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="mslavin",
+    username="code2college",
     password="iamnotspam",
-    hostname="mslavin.mysql.pythonanywhere-services.com",
-    databasename="mslavin$MyData")
+    hostname="code2college.mysql.pythonanywhere-services.com",
+    databasename="code2college$MyData")
+
+### START Week12 Day 1 demo code ###
+# @app.route('/', methods=["GET"])
+# def index():
+#     return render_template("week12day1examplecode.html")
+
+# @app.route('/carsearch', methods=["GET"])
+# def car_search():
+#     #print "Entering method car_search"
+
+#     year = request.args["year"] if request.args.has_key("year") else None
+#     make = request.args["make"] if request.args.has_key("make") else None
+#     model = request.args["model"] if request.args.has_key("model") else None
+
+#     #print "year: " + year + " make: " + make + " model: " + model
+
+#     result = {"message": "No results"}
+
+#     result["rows"] = get_cars_by(year, make, model)
+
+
+#     if result["rows"]:
+#         result["message"] = str(len(result["rows"])) + " results"
+
+#     return jsonify(result)
+
+### END Week12 Day 1 demo code ###
+
 
 @app.route('/', methods=["GET"])
 def index():
@@ -55,5 +84,8 @@ def create_query(name, category, url):
         else:
             query_string += " WHERE recipe_link LIKE %s".format(url)
         params += ("%"+url+"%",)
+    # we're adding this limit to cover the scenario the user didn't supply any
+    # paramters. We don't want to return ALL the rows in our DB!
+    query_string += " limit 1"
 
     return query_string, params
